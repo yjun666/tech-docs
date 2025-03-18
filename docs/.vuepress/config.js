2bf1dbb717c65cc5b870d68b0ad52c51bd814e59
yjun666 <15931501775@163.com>
2025-03-18 22:23:29 +0800

    添加文档

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,18 +1,30 @@
-import { defaultTheme } from '@vuepress/theme-default'
-import { defineUserConfig } from 'vuepress/cli'
-import { viteBundler } from '@vuepress/bundler-vite'
+import { defaultTheme } from "@vuepress/theme-default";
+import { defineUserConfig } from "vuepress/cli";
+import { viteBundler } from "@vuepress/bundler-vite";
 
 export default defineUserConfig({
-  lang: 'en-US',
+  lang: "en-US",
 
-  title: 'VuePress',
-  description: 'My first VuePress Site',
+  title: "VuePress",
+  description: "My first VuePress Site",
 
   theme: defaultTheme({
-    logo: 'https://vuejs.press/images/hero.png',
-
-    navbar: ['/', '/get-started'],
+    logo: "https://vuejs.press/images/hero.png",
+    // navbar: ['/', '/get-started'],
+    nav: [
+      { text: "首页", link: "/" },
+      { text: "前端", link: "/frontend/" },
+    ],
+    sidebar: {
+      "/frontend/": [
+        {
+          title: "Vue",
+          collapsable: false,
+          children: ["/frontend/vue/vue2.md", "/frontend/vue/vue3.md"],
+        },
+      ],
+    },
   }),
 
   bundler: viteBundler(),
-})
+});
